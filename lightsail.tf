e2a93f95decc2a351bf0f98fcd6f983558f80431
Jodi <jodian.creighton9@gmail.com>
2023-10-16 17:23:55 -0600

    ticket number

---
+++ lightsail.tf
@@ -0,0 +1,18 @@
+resource "aws_lightsail_instance" "example" {
+  name              = "example-instance"
+  availability_zone = "us-east-1a" # Change to your desired availability zone
+  blueprint_id      = "amazon_linux_2"
+  bundle_id         = "micro_2_0"
+
+  user_data = <<-EOF
+#!/bin/bash
+sudo yum install httpd -y
+sudo systemctl start httpd
+sudo systemctl enable httpd
+echo "<h1>This Server is created using Terraform </h1>" >> /var/www/html/index.html
+EOF
+}
+
+output "public_ip" {
+  value = aws_lightsail_instance.example.public_ip_address
+}
